8004b80d63b3d81a6b87bca8db4c823475fd7155
Yi-Mu "Enoch" Chen <yi-mu.chen@cern.ch>
2020-05-08 19:11:15 -0400

    Added logging controls

--- gui_control.py
+++ gui_control.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import datetime
+import logging
 from server import create_server_flask, socketio
 
 if __name__ == '__main__':
   app = create_server_flask()
-  socketio.run(app, host='0.0.0.0', port=9100, log_output=False)
+
+
+  socketio.run(app, host='0.0.0.0', port=9100)
+
+
