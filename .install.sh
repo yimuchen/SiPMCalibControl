623d39c1e4eedde73eb634233c2f241c0d57ac5f
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-06-02 14:33:45 +0200

    Fixing the CSS compilation script

--- .install.sh
+++ .install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Linking external objects to external directory
-ln -sf /opt/external/drs       ./external/drs
+ln -sf /opt/external/drs /srv/external/drs
 # ln -sf /opt/external/picoscope ./external/picoscope
 
 # Compiling C++ related components
-CXX=/usr/bin/g++ cmake         ./
+CXX=/usr/bin/g++ cmake ./
 CXX=/usr/bin/g++ cmake --build ./
-
-# Generating CSS files
-sass server/style.scss:style.css
-mv   style.css  server/style.css
